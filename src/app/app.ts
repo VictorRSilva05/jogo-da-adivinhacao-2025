cd758dbe4bc038060157be03bf3f0a12b77773d9
Victor R. Silva <victorr.silva005@gmail.com>
2025-09-29 15:21:48 -0300

    Implementado dificuldade e tentativas

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -6,7 +6,7 @@ import { FormsModule } from '@angular/forms';
   imports: [FormsModule],
   templateUrl: './app.html',
 })
-export class App implements OnInit {
+export class App {
   public numeroDigitado: number = 1;
   public numeroSecreto: number = 0;
 
@@ -14,11 +14,40 @@ export class App implements OnInit {
   public dicaNumeroMaiorQue: number = 1;
   public dicaNumeroMenorQue: number = 100;
 
-  ngOnInit(): void {
-    this.numeroSecreto = this.obterNumeroSecreto(1, 100);
-  }
+  public dificuldadeSelecionada?: string;
+  public tentativasRestantes: number = 0;
 
+  public selecionarDificuldade(dificuldade: string){
+    switch(dificuldade){
+      case 'Fácil':
+      this.numeroSecreto = this.obterNumeroSecreto(10);
+      this.dicaNumeroMenorQue = 10;
+      this.tentativasRestantes = 3;
+      break;
+
+      case 'Médio':
+      this.numeroSecreto = this.obterNumeroSecreto(50);
+      this.dicaNumeroMenorQue = 50;
+      this.tentativasRestantes = 6;
+      break;
+
+      case 'Dificil ':
+      this.numeroSecreto = this.obterNumeroSecreto(100);
+      this.dicaNumeroMenorQue = 100;
+      this.tentativasRestantes = 7;
+      break;
+    }
+
+    this.dificuldadeSelecionada = dificuldade;
+  }
   public adivinhar() {
+    this.tentativasRestantes--;
+
+    if(this.tentativasRestantes <= 0){
+      this.jogoEstaFinalizazdo = true;
+      return;
+    }
+
     if (this.numeroDigitado < this.numeroSecreto) this.dicaNumeroMaiorQue = this.numeroDigitado;
     else if (this.numeroDigitado > this.numeroSecreto)
       this.dicaNumeroMenorQue = this.numeroDigitado;
@@ -30,11 +59,11 @@ export class App implements OnInit {
     this.dicaNumeroMaiorQue = 1;
     this.dicaNumeroMenorQue = 100;
     this.jogoEstaFinalizazdo = false;
-    this.numeroSecreto = this.obterNumeroSecreto(1, 100);
+    this.dificuldadeSelecionada = undefined;
   }
 
-  private obterNumeroSecreto(min: number, max: number) {
-    const numeroAleatorio: number = Math.random() * 100;
+  private obterNumeroSecreto(max: number) {
+    const numeroAleatorio: number = Math.random() * (max - 1) + 1;
 
     const numeroSecreto = Math.floor(numeroAleatorio);
 
